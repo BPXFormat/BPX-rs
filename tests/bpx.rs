5a1e12f725359e404f9d05f08ba7085a48abf115
Yuri Edward <yuri6037@outlook.com>
2021-05-10 00:13:41 +0200

    Fixed unit tests

--- tests/bpx.rs
+++ tests/bpx.rs
@@ -1,5 +1,6 @@
 use bpx::encoder::Encoder;
 use bpx::decoder::Decoder;
+use bpx::Interface;
 
 #[test]
 fn attempt_write_empty_bpxp()
@@ -7,7 +8,7 @@ fn attempt_write_empty_bpxp()
     let mut encoder = Encoder::new(std::path::Path::new("./the_very_first_bpx.bpx")).unwrap();
     encoder.save().unwrap();
     let decoder = Decoder::new(std::path::Path::new("./the_very_first_bpx.bpx")).unwrap();
-    assert_eq!(decoder.main_header.section_num, 0);
-    assert_eq!(decoder.main_header.version, 1);
-    assert_eq!(decoder.main_header.file_size, 40);
+    assert_eq!(decoder.get_main_header().section_num, 0);
+    assert_eq!(decoder.get_main_header().version, 1);
+    assert_eq!(decoder.get_main_header().file_size, 40);
 }
