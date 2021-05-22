cc6c23bba25c8228a181646d8c5baf51659f8feb
Yuri Edward <yuri6037@outlook.com>
2021-05-22 21:20:01 +0200

    Added doc introduction

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,8 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+//! This library is the official implementation for the [BPX](https://gitlab.com/bp3d/bpx/bpx/-/blob/master/BPX_Format.pdf) container format.
+
 use std::vec::Vec;
 
 mod garraylen;
