fae990bce12cc9f5736a0ce2434baed246788299
Yuri Edward <yuri6037@outlook.com>
2021-10-10 17:18:13 +0200

    Updated doc for header module

--- src/builder.rs
+++ src/builder.rs
@@ -26,7 +26,7 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-//! High-level utilities to generate low-level file headers
+//! High-level utilities to generate low-level file headers.
 
 use crate::header::{
     MainHeader,
--- src/error.rs
+++ src/error.rs
@@ -26,7 +26,7 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-//! Error declarations
+//! Error declarations.
 
 use std::{
     convert::From,
