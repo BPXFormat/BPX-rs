d27c28d120a4a82b13f166614fb7ea9e58ab6f69
Yuri Edward <yuri6037@outlook.com>
2021-10-15 19:55:43 +0200

    Added doc for common BPXS exports

--- src/variant/shader/mod.rs
+++ src/variant/shader/mod.rs
@@ -36,44 +36,82 @@ pub use decoder::ShaderPackDecoder;
 pub use encoder::ShaderPackBuilder;
 pub use encoder::ShaderPackEncoder;
 
+/// The supported BPX version for this shader variant decoder/encoder.
 pub const SUPPORTED_VERSION: u32 = 0x2;
 
+/// The standard type for a shader section in a BPX Shader Package (type S).
 pub const SECTION_TYPE_SHADER: u8 = 0x1;
+
+/// The standard type for a symbol table section in a BPX Shader Package (type S).
 pub const SECTION_TYPE_SYMBOL_TABLE: u8 = 0x2;
+
+/// The standard type for an extended data section in a BPX Shader Package (type S).
 pub const SECTION_TYPE_EXTENDED_DATA: u8 = 0x3;
 
+/// Represents a shader in a BPXS.
 #[derive(Clone)]
 pub struct Shader
 {
-    stage: Stage,
-    data: Vec<u8>
+    /// The shader stage.
+    pub stage: Stage,
+
+    /// The shader data.
+    pub data: Vec<u8>
 }
 
+/// Enum of all supported shader targets by BPXS.
 #[derive(Copy, Clone)]
 pub enum Target
 {
+    /// DirectX 11
     DX11,
+
+    /// DirectX 12
     DX12,
+
+    /// OpenGL 3.3 (Core context)
     GL33,
+
+    /// OpenGL 4.0 (Core context)
     GL40,
+
+    /// Vulkan 1.0
     VK10,
+
+    /// Apple Metal
     MT,
+
+    /// Any rendering API. Useful if this is a shader assembly.
     Any
 }
 
+/// Enum of all types of BPXS.
 #[derive(Copy, Clone)]
 pub enum Type
 {
+    /// A shader assembly.
     Assembly,
+
+    /// A shader pipeline/program.
     Pipeline
 }
 
+/// Enum of all supported shader stages by BPXS.
 #[derive(Copy, Clone)]
 pub enum Stage
 {
+    /// Vertex shader stage.
     Vertex,
+
+    /// Hull/Tessellation Control Shader (TCS) stage.
     Hull,
+
+    /// Domain/Tessellation Evaluation Shader (TES) stage.
     Domain,
+
+    /// Geometry shader stage.
     Geometry,
+
+    /// Pixel/fragment shader stage.
     Pixel
 }
