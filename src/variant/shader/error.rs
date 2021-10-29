60eb0a376c571e9b83e3cf4dd3a2c29f09eaed26
Yuri Edward <yuri6037@outlook.com>
2021-10-29 23:31:02 +0200

    Updated shader module to new generation utility macros

--- src/variant/shader/error.rs
+++ src/variant/shader/error.rs
@@ -27,163 +27,92 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 use std::fmt::{Display, Formatter};
+use crate::macros::named_enum;
+use crate::macros::variant_error;
+use crate::macros::impl_err_conversion;
 
-/// Represents the context of an EOS error.
-#[derive(Debug)]
-pub enum EosContext
-{
-    /// Reached EOS while reading a shader.
-    Shader,
+named_enum!(
+    /// Represents the context of an invalid code.
+    InvalidCodeContext {
+        /// Invalid render API target code.
+        Target : "target",
 
-    /// Reached EOS while reading the symbol table.
-    SymbolTable
-}
+        /// Invalid shader pack type code.
+        Type : "type",
 
-impl EosContext
-{
-    pub fn name(&self) -> &'static str
-    {
-        return match self {
-            EosContext::Shader => "shader",
-            EosContext::SymbolTable => "symbol table"
-        };
+        /// Invalid shader stage type code.
+        Stage : "stage",
+
+        /// Invalid symbol type code.
+        SymbolType : "symbol type"
     }
-}
+);
 
-/// Enumerates possible missing sections.
-#[derive(Debug)]
-pub enum Section
-{
-    /// Missing strings section.
-    Strings,
+variant_error!(
+    E {
+        /// Reached EOS while reading a shader.
+        Shader : "shader",
 
-    /// Missing symbol table section.
-    SymbolTable,
-
-    /// Missing optional extended data section.
-    ExtendedData
-}
-
-impl Section
-{
-    pub fn name(&self) -> &'static str
-    {
-        return match self {
-            Section::Strings => "string",
-            Section::SymbolTable => "symbol table",
-            Section::ExtendedData => "extended data"
-        };
+        /// Reached EOS while reading the symbol table.
+        SymbolTable : "symbol table"
     }
-}
 
-/// Represents the context of an invalid code.
-#[derive(Debug)]
-pub enum InvalidCodeContext
-{
-    /// Invalid render API target code.
-    Target,
+    S {
+        /// Missing strings section.
+        Strings : "strings",
 
-    /// Invalid shader pack type code.
-    Type,
+        /// Missing symbol table section.
+        SymbolTable : "symbol table",
 
-    /// Invalid shader stage type code.
-    Stage,
-
-    /// Invalid symbol type code.
-    SymbolType
-}
-
-impl InvalidCodeContext
-{
-    pub fn name(&self) -> &'static str
-    {
-        return match self {
-            InvalidCodeContext::Target => "target",
-            InvalidCodeContext::Type => "type",
-            InvalidCodeContext::Stage => "stage",
-            InvalidCodeContext::SymbolType => "symbol type"
-        };
+        /// Missing optional extended data section.
+        ExtendedData : "extended data"
     }
-}
 
-/// Represents a BPXS read error.
-#[derive(Debug)]
-pub enum ReadError
-{
-    /// Low-level BPX decoder error.
-    Bpx(crate::error::ReadError),
+    /// Represents a BPXS read error.
+    R {
+        /// Invalid code.
+        ///
+        /// # Arguments
+        /// * the context.
+        /// * the coding byte.
+        InvalidCode(InvalidCodeContext, u8),
 
-    /// Describes an io error.
-    Io(std::io::Error),
+        /// Describes a missing section.
+        MissingSection(Section),
 
-    /// Describes a structured data error.
-    Sd(crate::sd::ReadError),
+        /// Describes an EOS (End Of Section) error while reading.
+        Eos(EosContext),
 
-    /// A section error.
-    Section(crate::section::Error),
+        /// A strings error.
+        Strings(crate::strings::ReadError),
 
-    /// A strings error.
-    Strings(crate::strings::ReadError),
-
-    /// Invalid code.
-    ///
-    /// # Arguments
-    /// * the context.
-    /// * the coding byte.
-    InvalidCode(InvalidCodeContext, u8),
-
-    /// Unsupported BPX version.
-    BadVersion(u32),
-
-    /// Unsupported BPX type code.
-    BadType(u8),
-
-    /// Describes a missing section.
-    MissingSection(Section),
-
-    /// Describes an EOS (End Of Section) error while reading.
-    Eos(EosContext)
-}
-
-impl From<std::io::Error> for ReadError
-{
-    fn from(e: std::io::Error) -> Self
-    {
-        return ReadError::Io(e);
+        /// Describes a structured data error.
+        Sd(crate::sd::ReadError)
     }
-}
 
-impl From<crate::error::ReadError> for ReadError
-{
-    fn from(e: crate::error::ReadError) -> Self
-    {
-        return ReadError::Bpx(e);
-    }
-}
+    /// Represents a BPXS write error.
+    W {
+        /// A strings error.
+        Strings(crate::strings::WriteError),
 
-impl From<crate::strings::ReadError> for ReadError
-{
-    fn from(e: crate::strings::ReadError) -> Self
-    {
-        return ReadError::Strings(e);
+        /// Describes a structured data error.
+        Sd(crate::sd::WriteError)
     }
-}
+);
 
-impl From<crate::sd::ReadError> for ReadError
-{
-    fn from(e: crate::sd::ReadError) -> Self
-    {
-        return ReadError::Sd(e);
+impl_err_conversion!(
+    ReadError {
+        crate::strings::ReadError => Strings,
+        crate::sd::ReadError => Sd
     }
-}
+);
 
-impl From<crate::section::Error> for ReadError
-{
-    fn from(e: crate::section::Error) -> Self
-    {
-        return ReadError::Section(e);
+impl_err_conversion!(
+    WriteError {
+        crate::strings::WriteError => Strings,
+        crate::sd::WriteError => Sd
     }
-}
+);
 
 impl Display for ReadError
 {
@@ -201,66 +130,6 @@ impl Display for ReadError
             ReadError::MissingSection(s) => f.write_str(&format!("missing {} section", s.name())),
             ReadError::Eos(ctx) => f.write_str(&format!("got EOS while reading {}", ctx.name()))
         }
-    }
-}
-
-/// Represents a BPXS write error.
-#[derive(Debug)]
-pub enum WriteError
-{
-    /// Low-level BPX encoder error.
-    Bpx(crate::error::WriteError),
-
-    /// Describes an io error.
-    Io(std::io::Error),
-
-    /// A strings error.
-    Strings(crate::strings::WriteError),
-
-    /// A section error.
-    Section(crate::section::Error),
-
-    /// Describes a structured data error.
-    Sd(crate::sd::WriteError)
-}
-
-impl From<std::io::Error> for WriteError
-{
-    fn from(e: std::io::Error) -> Self
-    {
-        return WriteError::Io(e);
-    }
-}
-
-impl From<crate::error::WriteError> for WriteError
-{
-    fn from(e: crate::error::WriteError) -> Self
-    {
-        return WriteError::Bpx(e);
-    }
-}
-
-impl From<crate::strings::WriteError> for WriteError
-{
-    fn from(e: crate::strings::WriteError) -> Self
-    {
-        return WriteError::Strings(e);
-    }
-}
-
-impl From<crate::section::Error> for WriteError
-{
-    fn from(e: crate::section::Error) -> Self
-    {
-        return WriteError::Section(e);
-    }
-}
-
-impl From<crate::sd::WriteError> for WriteError
-{
-    fn from(e: crate::sd::WriteError) -> Self
-    {
-        return WriteError::Sd(e);
     }
 }
 
