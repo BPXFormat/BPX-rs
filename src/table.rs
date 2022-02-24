864c83f5215dfac8cbf76c55a939150d4c52ac1f
Yuri Edward <yuri6037@outlook.com>
2022-02-24 19:36:45 +0100

    Updated table module to include new NamedItemTable designed for immutable string access

--- src/table.rs
+++ src/table.rs
@@ -29,6 +29,7 @@
 //! This module provides a lookup-table style implementation.
 
 use std::{collections::HashMap, ops::Index, slice::Iter};
+use once_cell::unsync::OnceCell;
 
 use crate::{core::Container, strings::StringSection};
 
@@ -149,5 +150,147 @@ impl<T: Item + Clone> ItemTable<T>
         }
         self.map = Some(map);
         Ok(())
+    }
+}
+
+/// Helper class to work with named items stored as arrays in a BPX container.
+pub struct NamedItemTable<T>
+{
+    list: Vec<T>,
+    map: OnceCell<HashMap<String, T>>
+}
+
+impl<T> NamedItemTable<T>
+{
+    /// Constructs a new NamedItemTable from a list of items.
+    ///
+    /// # Arguments
+    ///
+    /// * `list`: the list of items.
+    ///
+    /// returns: ItemTable<T>
+    pub fn with_list(list: Vec<T>) -> Self
+    {
+        Self { list, map: OnceCell::new() }
+    }
+
+    /// Constructs a new empty NamedItemTable.
+    pub fn empty() -> Self {
+        Self {
+            list: Vec::new(),
+            map: OnceCell::from(HashMap::new())
+        }
+    }
+
+    /// Gets all items in this table.
+    pub fn iter(&self) -> std::slice::Iter<T> {
+        self.list.iter()
+    }
+
+    /// Returns true if this table is empty.
+    pub fn is_empty(&self) -> bool {
+        self.list.is_empty()
+    }
+
+    /// Returns the number of items in this table.
+    pub fn len(&self) -> usize {
+        self.list.len()
+    }
+
+    /// Removes an item from this table.
+    ///
+    /// # Arguments
+    ///
+    /// * `index`: the index of the item to remove.
+    ///
+    /// returns: ()
+    pub fn remove(&mut self, index: usize) {
+        self.list.remove(index);
+    }
+}
+
+impl<T: Clone> NamedItemTable<T>
+{
+    /// Adds a new item to this table.
+    ///
+    /// # Arguments
+    ///
+    /// * `name`: the name of the item to add.
+    /// * `item`: the item to add.
+    ///
+    /// returns: ()
+    pub fn push(&mut self, name: String, item: T) -> usize {
+        if let Some(map) = self.map.get_mut() {
+            self.list.push(item.clone());
+            map.insert(name, item);
+        } else {
+            self.list.push(item);
+        }
+        self.len() - 1
+    }
+}
+
+impl<T: Item> NamedItemTable<T>
+{
+    /// Load the name of an item.
+    ///
+    /// # Arguments
+    ///
+    /// * `container`: the container to load the string from.
+    /// * `strings`: the string section to use for loading the string.
+    /// * `item`: the item to load the string of.
+    ///
+    /// returns: Result<&str, ReadError>
+    ///
+    /// # Errors
+    ///
+    /// A [ReadError](crate::strings::ReadError) is returned if the strings could not be loaded.
+    pub fn load_name<'a, T1>(&self, container: &Container<T1>, strings: &'a StringSection, item: &T) -> Result<&'a str, crate::strings::ReadError> {
+        strings.get(container, item.get_name_address())
+    }
+}
+
+impl<T: Item + Clone> NamedItemTable<T>
+{
+    fn build_lookup_table<T1>(&self, container: &Container<T1>, strings: &StringSection) -> Result<(), crate::strings::ReadError> {
+        let mut map: HashMap<String, T> = HashMap::new();
+        for v in &self.list {
+            let name = strings.get(container, v.get_name_address())?.into();
+            map.insert(name, v.clone());
+        }
+        self.map.set(map).unwrap_err();
+        Ok(())
+    }
+
+    /// Lookup an item by its name.
+    /// Returns None if the item does not exist.
+    ///
+    /// # Arguments
+    ///
+    /// * `container`: the container to load strings from if needed.
+    /// * `strings`: the string section to use for loading strings if needed.
+    /// * `name`: the name to search for.
+    ///
+    /// returns: Result<Option<&T>, ReadError>
+    ///
+    /// # Errors
+    ///
+    /// A [ReadError](crate::strings::ReadError) is returned if the strings could not be loaded.
+    pub fn find_by_name<T1>(&self, container: &Container<T1>, strings: &StringSection, name: &str) -> Result<Option<&T>, crate::strings::ReadError> {
+        if self.map.get().is_none() {
+            self.build_lookup_table(container, strings)?;
+        }
+        let map = unsafe { self.map.get().unwrap_unchecked() };
+        Ok(map.get(name))
+    }
+}
+
+impl<'a, T> IntoIterator for &'a NamedItemTable<T>
+{
+    type Item = &'a T;
+    type IntoIter = std::slice::Iter<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
     }
 }
