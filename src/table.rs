c2f22a178f7b55d1e1dbdd4e780fdc3be55e8e1d
Yuri Edward <yuri6037@outlook.com>
2022-02-26 11:10:57 +0100

    Removed old, now unused ItemTable

--- src/table.rs
+++ src/table.rs
@@ -28,7 +28,7 @@
 
 //! This module provides a lookup-table style implementation.
 
-use std::{collections::HashMap, ops::Index, slice::Iter};
+use std::collections::HashMap;
 use once_cell::unsync::OnceCell;
 
 use crate::{core::Container, strings::StringSection};
@@ -38,119 +38,6 @@ pub trait Item
 {
     /// Returns the address of the name of this item in its string section.
     fn get_name_address(&self) -> u32;
-}
-
-/// Represents an item table with on demand lookup capability (the lookup function only works after you've built it).
-pub struct ItemTable<T: Item>
-{
-    list: Vec<T>,
-    map: Option<HashMap<String, T>>
-}
-
-impl<T: Item> ItemTable<T>
-{
-    /// Constructs a new ItemTable from a list of items.
-    ///
-    /// # Arguments
-    ///
-    /// * `list`: the list of items.
-    ///
-    /// returns: ItemTable<T>
-    pub fn new(list: Vec<T>) -> Self
-    {
-        Self { list, map: None }
-    }
-
-    /// Gets all items in this table.
-    pub fn iter(&self) -> Iter<T>
-    {
-        self.list.iter()
-    }
-
-    /// Returns the number of items in this table.
-    pub fn len(&self) -> usize
-    {
-        self.list.len()
-    }
-
-    /// Returns true if this table is empty.
-    pub fn is_empty(&self) -> bool
-    {
-        self.list.is_empty()
-    }
-
-    /// Lookup an item by its name.
-    /// Returns None if the item does not exist.
-    ///
-    /// # Arguments
-    ///
-    /// * `name`: the name of the item to search for.
-    ///
-    /// returns: Option<&Self::Inner>
-    ///
-    /// # Panics
-    ///
-    /// Panics if the lookup table is not yet built.
-    pub fn lookup(&self, name: &str) -> Option<&T>
-    {
-        if let Some(map) = &self.map {
-            map.get(name)
-        } else {
-            panic!("Lookup table has not yet been initialized, please call build_lookup_table");
-        }
-    }
-}
-
-impl<'a, T: Item> IntoIterator for &'a ItemTable<T>
-{
-    type Item = &'a T;
-    type IntoIter = <&'a Vec<T> as IntoIterator>::IntoIter;
-
-    fn into_iter(self) -> Self::IntoIter
-    {
-        self.list.iter()
-    }
-}
-
-impl<T: Item> Index<usize> for ItemTable<T>
-{
-    type Output = T;
-
-    fn index(&self, index: usize) -> &Self::Output
-    {
-        &self.list[index]
-    }
-}
-
-impl<T: Item + Clone> ItemTable<T>
-{
-    /// Builds the item map for easy and efficient lookup of items by name.
-    ///
-    /// **You must call this function before you can use lookup.**
-    ///
-    /// # Arguments
-    ///
-    /// * `names`: the NameTable to load the names from.
-    ///
-    /// returns: Result<(), Error>
-    ///
-    /// # Errors
-    ///
-    /// An [Error](crate::strings::Error) is returned if the strings could not be loaded.
-    pub fn build_lookup_table<T1>(
-        &mut self,
-        container: &mut Container<T1>,
-        names: &mut StringSection
-    ) -> Result<(), crate::strings::Error>
-    {
-        let mut map: HashMap<String, T> = HashMap::new();
-        for v in &self.list {
-            let name = names.get(container, v.get_name_address())?.into();
-            map.insert(name, v.clone());
-        }
-        self.map = Some(map);
-        Ok(())
-    }
 }
 
 /// Helper class to work with named items stored as arrays in a BPX container.
