7bb2e75fb4d98c27ce851e52d7bde505e1a6bda3
Yuri Edward <yuri6037@outlook.com>
2022-02-27 21:07:57 +0100

    Use of undocumented weird code to enable feature documentation on docs.rs

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,8 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+
 #![warn(missing_docs)]
 
 //! This library is the official implementation for the [BPX](https://gitlab.com/bp3d/bpx/bpx/-/blob/rev2/BPX_Format.pdf) container format.
