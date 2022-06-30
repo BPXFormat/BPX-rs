3dc060ba457b170a49905a24108e54da8ecef9cf
Yuri Edward <yuri6037@outlook.com>
2022-06-30 21:45:58 +0200

    Added shift function to SectionData with unit tests.

--- src/core/data/mod.rs
+++ src/core/data/mod.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, BlockProject 3D
+// Copyright (c) 2022, BlockProject 3D
 //
 // All rights reserved.
 //
@@ -36,6 +36,33 @@ use std::{
     io::{Read, Result, Seek, Write},
     vec::Vec,
 };
+use std::io::SeekFrom;
+
+#[cfg(not(test))]
+const SHIFT_BUF_SIZE: usize = 8192;
+#[cfg(test)]
+const SHIFT_BUF_SIZE: usize = 4;
+
+/// Shift direction and amount for shifting section data.
+pub enum Shift {
+    /// Shift to the left.
+    Left(u32),
+
+    /// Shift to the right.
+    Right(u32)
+}
+
+/// Write mode for [SectionData](crate::core::data::SectionData).
+pub enum WriteMode {
+    /// Write data by appending bytes at the current cursor location.
+    Append,
+
+    /// Write data over existing data if any.
+    ///
+    /// This is the behavior commonly expected by most [Write](std::io::Write) implementations,
+    /// as such this is also the default for [SectionData](crate::core::data::SectionData).
+    Overwrite
+}
 
 /// Opaque variant intended to manipulate section data in the form of standard IO operations.
 pub trait SectionData: Read + Write + Seek {
@@ -50,8 +77,143 @@ pub trait SectionData: Read + Write + Seek {
         Ok(data)
     }
 
+    /// Shifts all bytes after cursor, in the section, to the left or to the right.
+    ///
+    /// If this operation fails the data in the section may appear partially shifted.
+    ///
+    /// # Arguments
+    ///
+    /// * `shift`: the shift direction and length.
+    ///
+    /// returns: Result<(), Error>
+    ///
+    /// # Errors
+    ///
+    /// An [Error](std::io::Error) is returned if a read, write or seek operation has failed.
+    fn shift(&mut self, shift: Shift) -> Result<()> {
+        let cursor = self.stream_position()?;
+        let mut buf = [0; SHIFT_BUF_SIZE];
+        match shift {
+            Shift::Left(mut length) => {
+                if length > cursor as u32 {
+                    length = cursor as u32;
+                }
+                let mut destination = cursor - length as u64;
+                let mut source = cursor;
+                loop {
+                    self.seek(SeekFrom::Start(source))?;
+                    let len = self.read_fill(&mut buf)?;
+                    if len == 0 {
+                        break;
+                    }
+                    source += len as u64;
+                    self.seek(SeekFrom::Start(destination))?;
+                    self.write_all(&buf[..len])?;
+                    destination += len as u64;
+                }
+            },
+            Shift::Right(length) => {
+                let size = self.size() as u64;
+                let mut source = size;
+                let mut destination = source + length as u64;
+                while source > cursor {
+                    let nextsize = std::cmp::min(SHIFT_BUF_SIZE as u64, source - cursor);
+                    self.seek(SeekFrom::Start(source - nextsize))?;
+                    self.read_exact(&mut buf[..nextsize as usize])?;
+                    self.seek(SeekFrom::Start(destination - nextsize))?;
+                    self.write_all(&buf[..nextsize as usize])?;
+                    source -= nextsize;
+                    destination -= nextsize;
+                }
+            }
+        }
+        Ok(())
+    }
+
     /// Returns the current size of this section.
     fn size(&self) -> usize;
 }
 
 pub use auto::AutoSectionData;
+use crate::utils::ReadFill;
+
+#[cfg(test)]
+mod tests {
+    use std::io::{Read, Seek, SeekFrom, Write};
+    use crate::core::{AutoSectionData, SectionData};
+    use crate::core::data::Shift;
+
+    const SEED: &str = "This is a test.";
+
+    #[test]
+    fn basic_shift_left() {
+        let mut data = AutoSectionData::new();
+        data.write_all(SEED.as_bytes()).unwrap();
+        data.seek(SeekFrom::End(-4)).unwrap();
+        data.shift(Shift::Left(2)).unwrap();
+        data.seek(SeekFrom::Start(0)).unwrap();
+        let mut buf = [0; SEED.len()];
+        data.read(&mut buf).unwrap();
+        assert_eq!(std::str::from_utf8(&buf).unwrap(), "This is aest.t.");
+    }
+
+    #[test]
+    fn basic_shift_right() {
+        let mut data = AutoSectionData::new();
+        data.write_all(SEED.as_bytes()).unwrap();
+        data.seek(SeekFrom::End(-4)).unwrap();
+        data.shift(Shift::Right(2)).unwrap();
+        data.seek(SeekFrom::Start(0)).unwrap();
+        let mut buf = [0; SEED.len() + 2];
+        data.read(&mut buf).unwrap();
+        assert_eq!(std::str::from_utf8(&buf).unwrap(), "This is a tesest.");
+    }
+
+    #[test]
+    fn long_shift_left() {
+        let mut data = AutoSectionData::new();
+        data.write_all(SEED.as_bytes()).unwrap();
+        data.seek(SeekFrom::Start(4)).unwrap();
+        data.shift(Shift::Left(2)).unwrap();
+        data.seek(SeekFrom::Start(0)).unwrap();
+        let mut buf = [0; SEED.len()];
+        data.read(&mut buf).unwrap();
+        assert_eq!(std::str::from_utf8(&buf).unwrap(), "Th is a test.t.");
+    }
+
+    #[test]
+    fn long_shift_right() {
+        let mut data = AutoSectionData::new();
+        data.write_all(SEED.as_bytes()).unwrap();
+        data.seek(SeekFrom::Start(4)).unwrap();
+        data.shift(Shift::Right(2)).unwrap();
+        data.seek(SeekFrom::Start(0)).unwrap();
+        let mut buf = [0; SEED.len() + 2];
+        data.read(&mut buf).unwrap();
+        assert_eq!(std::str::from_utf8(&buf).unwrap(), "This i is a test.");
+    }
+
+    #[test]
+    fn zero_shift_left() {
+        let mut data = AutoSectionData::new();
+        data.write_all(SEED.as_bytes()).unwrap();
+        data.seek(SeekFrom::Start(0)).unwrap();
+        data.shift(Shift::Left(2)).unwrap();
+        data.seek(SeekFrom::Start(0)).unwrap();
+        let mut buf = [0; SEED.len()];
+        data.read(&mut buf).unwrap();
+        assert_eq!(std::str::from_utf8(&buf).unwrap(), "This is a test.");
+    }
+
+    #[test]
+    fn zero_shift_right() {
+        let mut data = AutoSectionData::new();
+        data.write_all(SEED.as_bytes()).unwrap();
+        data.seek(SeekFrom::Start(0)).unwrap();
+        data.shift(Shift::Right(2)).unwrap();
+        data.seek(SeekFrom::Start(0)).unwrap();
+        let mut buf = [0; SEED.len() + 2];
+        data.read(&mut buf).unwrap();
+        assert_eq!(std::str::from_utf8(&buf).unwrap(), "ThThis is a test.");
+    }
+}
